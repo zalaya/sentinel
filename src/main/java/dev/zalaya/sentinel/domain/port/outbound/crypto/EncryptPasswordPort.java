73799e0887d1fe4a0c5f468e91e45f82032a2b72
Zalaya <enol.corzal@gmail.com>
2025-03-24 20:51:47 +0100

    feat: add the structure of the project and the packages

--- src/main/java/dev/zalaya/sentinel/domain/port/outbound/crypto/EncryptPasswordPort.java
+++
@@ -1,7 +0,0 @@
-package dev.zalaya.sentinel.domain.port.outbound.crypto;
-
-public interface EncryptPasswordPort {
-
-    String encrypt(String raw);
-
-}
--- src/main/java/dev/zalaya/sentinel/domain/port/outbound/crypto/VerifyPasswordPort.java
+++
@@ -1,7 +0,0 @@
-package dev.zalaya.sentinel.domain.port.outbound.crypto;
-
-public interface VerifyPasswordPort {
-
-    boolean verify(String raw, String encrypted);
-
-}
